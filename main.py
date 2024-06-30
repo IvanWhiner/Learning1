eca64275fa44ec09f8902d864ecfe9cd63300e46
IvanWhiner <vanyadd17@gmail.com>
2024-06-30 14:22:08 +0300

    баловство

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
+from tkinter import *
+from tkinter import  ttk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class FeetToMeters:
+
+    def __init__(self, root):
+
+        root.title('Футы в метры')
+
+        s1 = ttk.Style()
+        s1.configure('Danger.TFrame',  background='red', relief='sunken')
+
+        # mainframe = ttk.Frame(root, padding= (5, 10), style='Danger.TFrame')
+        mainframe = ttk.Frame(root, padding=(5, 10))
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe['relief'] = 'groove'
+
+        frame2 = ttk.Frame(root, padding=(5, 10))
+        frame2.grid(column=1, row=0, sticky=(S))
+
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        #Здесь буду пытаться все ломать
+
+        ttk.Label(frame2, text='Halk lomat'
+                               '\ni krushit').grid(column=1, row=0, sticky = W)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        self.feet = StringVar()
+        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
+        feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+        self.meters = StringVar()
+        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
+
+        ttk.Button(mainframe, text='Вычислить', command=self.calculate).grid(column=3, row=3, sticky=(W))
+
+        ttk.Label(mainframe, text='Фут').grid(column=3, row=1, sticky=W)
+        ttk.Label(mainframe, text='равны').grid(column=1, row=2, sticky=E)
+        ttk.Label(mainframe, text='метров').grid(column=3, row=2, sticky=W)
+
+        for child in mainframe.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+        feet_entry.focus()
+        root.bind("<Return>", self.calculate)
+
+    def calculate(self, *args):
+        try:
+            value = float(self.feet.get())
+            self.meters.set(int(0.3048 * value * 10000.0 + 0.5) / 10000.0)
+        except ValueError:
+            pass
+
+def print_hierarchy(w, depth=0):
+    print('  '*depth + w.winfo_class() + ' w=' + str(w.winfo_width()) + ' h=' + str(w.winfo_height()) + ' x=' + str(w.winfo_x()) + ' y=' + str(w.winfo_y()))
+    for i in w.winfo_children():
+        print_hierarchy(i, depth+1)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+root = Tk()
+# text = ttk.Label(root, text="Hello World!")
+# text.grid()
+# l =ttk.Label(root, text="Starting...")
+# l.grid()
+# l.bind('<1>', lambda e: l.configure(text='B1 нажато'))
+# l.bind_all('<Shift_L><A>', lambda e: l.configure(text='Левый шифт нажат'))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+FeetToMeters(root)
+# print_hierarchy(root)
+root.mainloop()
+
